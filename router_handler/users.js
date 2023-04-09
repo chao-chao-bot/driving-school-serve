a9124bccafb8eb60b1143a95185b18a5ea60042e
xiachao <1425894912@qq.com>
2023-04-09 09:42:25 +0800

    feat: 登录接口完成

--- router_handler/users.js
+++ router_handler/users.js
@@ -2,20 +2,21 @@ const db = require('../db/index')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('../config')
+
+const userMap = {
+  admin:"admin",
+  coach:"coach",
+  student:"student"
+}
 exports.regUser = (req, res) => {
   // 接收表单数据
-  
-  const {username:name,password,usertype:type} = req.body
+  let {username:name,password,usertype:type} = req.body
   console.log(req.body);
   // 判断数据是否合法
   if (!name || !password || !type) {
     return res.esend('用户名或密码不能为空！')
   }
-  const userMap = {
-    admin:"admin",
-    coach:"coach",
-    student:"student"
-  }
+  
   const userSqlTable = userMap[type]
   const sql = `select * from ${userSqlTable} where name=?`
   db.query(sql, [name], function (err, results) {
@@ -27,9 +28,9 @@ exports.regUser = (req, res) => {
       return res.esend('用户名被占用，请更换其他用户名！')
     }
     //注册用户
-    const hashPassword = bcrypt.hashSync(password, 10)
+    //  password = bcrypt.hashSync(password, 10)
     const sql = `insert into ${userSqlTable} set ?`
-    db.query(sql, { name, password:hashPassword }, function (err, results) {
+    db.query(sql, { name, password }, function (err, results) {
       // 执行 SQL 语句失败
       if (err) return res.esend(err)
 
@@ -47,6 +48,40 @@ exports.regUser = (req, res) => {
         id: results.insertId,
       })
     })
+  })
+}
+
+// 登录的处理函数
+exports.login = (req, res) => {
+  // 接收表单数据
+  const {username:name,password,usertype:type} = req.body
+  // 判断数据是否合法
+  if (!name || !password || !type) {
+    return res.esend('用户名或密码不能为空！')
+  }
+  const userSqlTable = userMap[type]
+  const sql = `select * from ${userSqlTable} where name=?`
+  db.query(sql, name,  function (err, results) {
+    if (err) return res.esend(err)
+    if (results.length !== 1) return res.esend('登录失败,请检查身份、账号和密码')
+    // const compareResult = bcrypt.compareSync(password,results[0].password)
+    const compareResult = results[0].password === password
+    console.log(compareResult,password);
+    if (!compareResult) {
+      return res.esend('登录失败,请检查账号和密码')
+    }
+    const user = { ...results[0], password: '' }
+    const tokenStr = jwt.sign(user, config.jwtSecretKey, {
+      expiresIn: '40h',
+    })
+    setTimeout(() => {
+      res.ssend({
+        token: 'Bearer ' + tokenStr,
+        name: results[0].name,
+        id: results[0].id,
+      })
+    }, 1000)
+
   })
 }
 
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const userHandler = require('../router_handler/users')
 
 // 注册新用户
 router.post('/auth/register', userHandler.regUser)
+router.post('/auth/login', userHandler.login)
 
 module.exports = router;
