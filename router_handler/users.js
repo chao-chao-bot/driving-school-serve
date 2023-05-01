e5e8ebc99b0a75beba4fd76dce998ab6be933825
xiachao <1425894912@qq.com>
2023-05-01 14:51:06 +0800

    feat: 注册完成

--- router_handler/users.js
+++ router_handler/users.js
@@ -3,22 +3,14 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('../config')
 
-const userMap = {
-  admin:"admin",
-  coach:"coach",
-  student:"student"
-}
+
 exports.regUser = (req, res) => {
   // 接收表单数据
-  let {username:name,password,usertype:type} = req.body
-  console.log(req.body);
-  // 判断数据是否合法
-  if (!name || !password || !type) {
+  let {name,password} = req.body
+  if (!name || !password ) {
     return res.esend('用户名或密码不能为空！')
   }
-  
-  const userSqlTable = userMap[type]
-  const sql = `select * from ${userSqlTable} where name=?`
+  const sql = `select * from users where name=?`
   db.query(sql, [name], function (err, results) {
     if (err) {
       return res.esend(err)
@@ -27,14 +19,9 @@ exports.regUser = (req, res) => {
     if (results.length > 0) {
       return res.esend('用户名被占用，请更换其他用户名！')
     }
-    //注册用户
-    //  password = bcrypt.hashSync(password, 10)
-    const sql = `insert into ${userSqlTable} set ?`
+    const sql = `insert into users set ?`
     db.query(sql, { name, password }, function (err, results) {
-      // 执行 SQL 语句失败
       if (err) return res.esend(err)
-
-      // SQL 语句执行成功，但影响行数不为 1
       if (results.affectedRows !== 1) {
         return res.esend('注册用户失败，请稍后再试！')
       }
